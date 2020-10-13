8ceddc996710cd012915df22a69be8599fa9b987
Darren Tebo <darrentebo83@outlook.com>
2020-10-13 15:54:06 -0400

    added routing to the app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+
+import FriendsList from './components/Friends/FriendsList';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/protected'>Friends List</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path='/protected' component={FriendsList} />
+          <Route path='/login' component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/Friends/FriendsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsList = props => {
+    return(
+        <>
+            <h1>FriendsList here...</h1>
+        </>
+    );
+};
+
+export default FriendsList;
